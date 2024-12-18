d1e8d07e467a8099e3fe252df3e7cb5dcc1d6c17
simon <simon@MORIARTY>
2024-12-18 16:24:14 +0100

    Add project files.

---
+++ ZimniKapitola/Gift.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZimniKapitola
+{
+    public class Gift
+    {
+        public string futureOwner {  get; set; }
+        public bool isHidden { get; set; }
+        HidingSpots hs = new HidingSpots();
+        public void hide()
+        {
+            if(!hs.spots.Equals(null) && !isHidden)
+            {
+                //function where the gift will be hidden in the most efficient place
+            }
+        }
+    }
+}
